617ca01ceaf6997ac6627c0526201174c05d0330
uhyun-fe <feyouhyun0957@gmail.com>
2021-08-17 10:41:41 +0900

    update : update test result text - 유형 결과 텍스트 업데이트 - 유형별 이미지 추가

---
+++ src/lib/api.js
@@ -0,0 +1,16 @@
+// import axios from "axios";
+
+// const WAYTEST_SERVER_URI = "http://localhost:5000/api/v1/";
+
+// const api = axios.create({
+//    baseURL: WAYTEST_SERVER_URI,
+//    headers: {
+//       accept: "application/json",
+//       "Content-Type": "multipart/form-data",
+//       "Access-Control-Allow-Origin": "*",
+//    },
+// });
+
+// export const apiQuest = {
+//    getQuestList: () => api.get("restaurants"),
+// };
